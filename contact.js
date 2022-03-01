ed1927dc4317e153f030e5f39895e60eff1db746
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-03-01 10:56:35 +0000

    add readline

---
+++ contact.js
@@ -0,0 +1,13 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+{
+    "contacts":{
+         "created_contact":{
+            "name": "name",
+            "number": "number",
+            "email": "email"
+        } // this will be repeated for every contact obviously with real data.
+    }, 
+}
