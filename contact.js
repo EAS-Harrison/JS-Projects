f55654d37e414726d54cbfa671c029320e9520f9
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-03-01 15:06:24 +0000

    change input method

--- contact.js
+++ contact.js
@@ -1,13 +1,25 @@
+const inquirer = require('inquirer')
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   })
-{
-    "contacts":{
+  {
+    var contacts = [{
          "created_contact":{
-            "name": "name",
-            "number": "number",
-            "email": "email"
-        } // this will be repeated for every contact obviously with real data.
-    }, 
-}
+            "name": "",
+            "number": "",
+            "email": "" }
+        }] // this will be repeated for every contact obviously with real data.
+
+var questions = [
+    {
+      type: 'input',
+      name: 'name',
+      number: 'number',
+      email: 'email',
+      message: "What are your details?",
+    }
+  ]
+  inquirer.prompt(questions).then(answers => {
+    console.log(`Hi ${answers['name']} ${answers['number']}!`)
+  }}
