0b8c5a9525610477a7923f0be4c82b028272f812
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-03-02 09:31:58 +0000

    add switch

--- contact.js
+++ contact.js
@@ -1,31 +1,38 @@
+const { create } = require('domain')
 const inquirer = require('inquirer')
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   })
   {
-    var contacts = [{
-         "created_contact":{
-            "name": "",
-            "number": "",
-            "email": "" }
-        }] // this will be repeated for every contact obviously with real data.
+    var contacts = [] // this will be repeated for every contact obviously with real data.
 
         inquirer
-        .prompt([
-          {
-            name: 'name',
-            message: 'What is your contacts name?',
-          },
-          {
-            name: 'email',
-            message: 'What is your contacts email?',
-          },
-          {
-            name: 'number',
-            message: 'What is your contacts number?',
-          },
-        ])
-        .then(answers => {
-          console.log(`Name: ${answers["name"]} , Number: ${answers["number"]} , Email: ${answers["email"]}`)
-        })}
+        .prompt("Do you want to: Create,Update,or Delete?")
+        .then(answers => {}
+        )}
+        switch(method) {
+            case "Create":
+              create()
+              break;
+            case "Update":
+              update()
+              break;
+            case remove:
+              remove()
+            break;  
+            default:
+                console.log('Not an option')
+                return;
+        }
+        const create = () => {
+            console.log('Create a contact')
+        }
+        
+        const update = () => {
+            console.log('Update a contact')
+        }
+        
+        const remove = () => {
+            console.log('Remove a contact')
+        }
