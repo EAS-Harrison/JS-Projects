7c1430f5b54495262e993cdac4d810c8f39b198a
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-03-02 11:31:52 +0000

    added if statements to the choice

--- contact.js
+++ contact.js
@@ -3,35 +3,44 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   })
-  {
+  
     var contacts = [] // this will be repeated for every contact obviously with real data.
+const main = async () => {
+    const { choice } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What do you want to do?',
+            choices: [
+                'Create a contact',
+                'Update a contact',
+                'Delete a contact',
+            ],
+        }
+    ])
+    console.log(choice)
+}
 
-        inquirer
-        .prompt("Do you want to: Create,Update,or Delete?")
-        .then(answers => {}
-        )}
-        switch(method) {
-            case "Create":
-              create()
-              break;
-            case "Update":
-              update()
-              break;
-            case remove:
-              remove()
-            break;  
-            default:
-                console.log('Not an option')
-                return;
-        }
-        const create = () => {
-            console.log('Create a contact')
-        }
-        
-        const update = () => {
-            console.log('Update a contact')
-        }
-        
-        const remove = () => {
-            console.log('Remove a contact')
-        }
+main()
+      
+if ((choice) = ('Create a contact')){
+  inquirer
+  .prompt([
+    {
+      name: 'name',
+      message: 'What is your contacts name?',
+    },
+    {
+      name: 'number',
+      message: 'What is your contacts number?',
+    },
+    {
+      name: 'email',
+      message: 'What is your contacts email?',
+    },
+  
+  ])
+  .then(answers => {
+    console.log(`Name: ${answers['name']} Number: ${answers['number']} Email: ${answers['email']}`);
+  });
+}
