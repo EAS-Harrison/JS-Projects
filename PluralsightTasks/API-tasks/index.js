b989ac5ab0f92d04c7de89240146c3105063e2c2
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-04-13 14:58:28 +0100

    made progress with the API methods

--- PluralsightTasks/API-tasks/index.js
+++ PluralsightTasks/API-tasks/index.js
@@ -1,14 +1,51 @@
 let express = require('express')
 let app = express()
+let pieRepo = require('./repos/pieRepo');
 
 let router = express.Router()
 
+
 router.get('/', function (req, res, next) {
-    res.send('Apple')
+    pieRepo.get(function (data) {
+        res.status(200).json({
+            'status': 200,
+            'statusText': 'OK',
+            'message': 'All pies retrieved',
+            'data': data
+        })
+    },
+        function (err) {
+            next(err)
+        })
 })
+router.get('/:id', function (req, res, next) {
+    pieRepo.getById(req.params.id, function (data) {
+        if (data) {
+            res.status(200).json({
+                'status': 200,
+                'statusText': 'OK',
+                'message': 'All pies retrieved',
+                'data': data
+            })
+        } else {
+            res.status(404).json({
+                'status': 404,
+                'statusText': 'Not found',
+                'message': `The pie '${req.params.id}' could not be found.`,
+                'error': {
+                    "code": "NOT_FOUND",
+                    "message": `The pie '${req.params.id}' could not be found.`
+                }
+            })
+        }
+    }, function (err) {
+        next(err)
+    })
+})
+
 
 app.use('/api/', router)
 
-var server = app.listen(5000, function () {
-    console.log('Node server is running on localhost:5000')
+var server = app.listen(3000, function () {
+    console.log('Node server is running on localhost:3000')
 })
